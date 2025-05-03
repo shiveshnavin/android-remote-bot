7d0227a6ccbbe28454289951a43ad1dbaead369d
Shivesh Navin <you@example.com>
2025-05-03 21:47:13 +0530

    Fixes

--- dist/pipelane-server/client/app/home/[pipe]/index.d.ts
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export default function PipelanePage(): React.JSX.Element;
--- dist/pipelane-server/client/app/settings/index.d.ts
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export default function ProfilePage(): React.JSX.Element;
