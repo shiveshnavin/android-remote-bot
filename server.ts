a5d7d732d8b0563096f74a3f1674c937e9c2cb95
Shivesh Navin <you@example.com>
2025-05-03 21:31:03 +0530

    Update multi

--- server.ts
+++ server.ts
@@ -1,14 +1,24 @@
 import express from 'express'
 import { creatPipelaneServer } from './pipelane-server'
 import { FireStoreDB } from 'multi-db-orm'
-
+import fs from 'fs'
 const app = express()
-
-const db = new FireStoreDB(JSON.parse(process.env.FIREBASE_SEVICE_ACCOUNT_JSON as string))
+const firebaseCreds = fs.readFileSync('firebase-creds.json').toString()
+const dbCreds = JSON.parse(firebaseCreds)
+const db = new FireStoreDB(dbCreds)
 const PORT = 8080
+
+
+app.use('/bot', (req, res) => {
+    res.send({
+        status: 'Bot up'
+    })
+})
+
 creatPipelaneServer({}, db).then(pipelaneApp => {
-    app.use(pipelaneApp)
+    app.use('/pipelane', pipelaneApp)
     app.listen(PORT, () => {
         console.log('Android bot server started. Listening on port', PORT)
     })
 })
+
