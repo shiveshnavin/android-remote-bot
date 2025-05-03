777c4fd1a0f5d89b5228c44705a00a8ac0e9ed71
Shivesh Navin <you@example.com>
2025-05-03 22:28:53 +0530

    Fixes

---
+++ copy-files.sh
@@ -0,0 +1,3 @@
+cp pipelane-server/model.graphql dist/pipelane-server/model.graphql
+mkdir -p ./dist/pipelane-server/client
+cp -r ./ui-dist/ ./dist/pipelane-server/client/dist
--- dist/server.js
+++ dist/server.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const pipelane_server_1 = require("./pipelane-server");
 const multi_db_orm_1 = require("multi-db-orm");
 const fs_1 = __importDefault(require("fs"));
+const pipe_tasks_1 = require("./pipelane-server/server/pipe-tasks");
 const app = (0, express_1.default)();
 const firebaseCreds = fs_1.default.readFileSync('firebase-creds.json').toString();
 const dbCreds = JSON.parse(firebaseCreds);
@@ -17,7 +18,7 @@ app.use('/bot', (req, res) => {
         status: 'Bot up'
     });
 });
-(0, pipelane_server_1.creatPipelaneServer)({}, db).then(pipelaneApp => {
+(0, pipelane_server_1.creatPipelaneServer)(pipe_tasks_1.VariantConfig, db).then(pipelaneApp => {
     app.use('/pipelane', pipelaneApp);
     app.listen(PORT, () => {
         console.log('Android bot server started. Listening on port', PORT);
