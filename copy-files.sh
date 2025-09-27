cd5cbbb65e6f143a7f0b2f152e58cdf9a26cadc7
shiveshnavin <shiveshnavin@gmail.com>
2025-09-27 20:27:11 +0530

    Updates

--- copy-files.sh
+++ copy-files.sh
@@ -1,4 +1,4 @@
 cp pipelane-server/model.graphql dist/pipelane-server/model.graphql
 mkdir -p ./dist/pipelane-server/client
-# cp -r ./ui-dist/ ./dist/pipelane-server/client/dist || true
+cp -r ./precompiled-ui-dist/ ./dist/pipelane-server/client/dist || true
 echo Copied files success...
